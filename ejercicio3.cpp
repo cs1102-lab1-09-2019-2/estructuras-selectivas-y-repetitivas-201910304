75d89a7656c80d85dcc831ffa98bce79853d121d
201910304 <54292881+201910304@users.noreply.github.com>
2019-09-03 22:41:40 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,36 @@
+#include<iostream>
+#include<vector>
+using std::cout;
+using std::cin;
+using std::vector;
+using std::ostream;
 
+int numeros(int g){
+  vector<int>mayores;
+  int mayor = 0;
+  int indice;
+  for(int rango=0;rango<g;rango++)
+  {
+    cout<<"Agregue: ";
+    cin>>indice;
+    mayores.push_back(indice);
+  }
+  
+  for(auto i:mayores)
+  {
+    if(i>mayor)
+    {
+    mayor=i;
+    }
+  }
+  return mayor;
+}
 
-int main() {
-
-    return 0;
+int main()
+{ 
+  int n;
+  cout<<"Ingrese la n:";
+  cin>>n;
+  int mayor_de_la_lista=numeros(n);
+  cout<<"El número mayor es: " << mayor_de_la_lista<<"\n";
 }
